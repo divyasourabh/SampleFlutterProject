5f0dbfc0f332b8ecf64161a6b4d0a971bcada2ee
divyasourabh01 <divyasourabh.aswal@byjus.com>
2023-08-05 22:09:58 +0530

    HomePage Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,16 @@
 import 'package:flutter/material.dart';
 
+import 'homepage.dart';
+
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
-  // const MyApp(Key key) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: SafeArea(
-          child: Center(
-            child: Container(
-              child: Text("Welcome to Flutter"),
-            )
-        )
-        )
+        home: HomePage()
     );
   }
 }
